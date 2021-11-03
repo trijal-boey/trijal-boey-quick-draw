819aacc857a164f87d0b00feaa1c95a4c5f4b6e4
trijal-boey <89288580+trijal-boey@users.noreply.github.com>
2021-11-03 14:43:30 +0530

    Add files via upload

---
+++ New folder/dist/main.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function tp() {
+  window.location = "sketch.html";
+}
+
+function setup() {
+  canvas = createCanvas(200, 200);
+  canvas.center();
+  background("white");
+}
+
+function clearCanvas() {
+  background("white");
+}
+//# sourceMappingURL=main.dev.js.map
---
+++ New folder/main.js
@@ -0,0 +1,12 @@
+function tp(){
+    window.location="sketch.html";
+}
+function setup() {
+    canvas=createCanvas(200,200);
+    canvas.center()
+    background("white");
+
+}
+function clearCanvas(){
+    background("white");
+}
